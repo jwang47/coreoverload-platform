c366eb6d07a66bc44fc7415e5c74d1052540ee13
Jiahua Wang <jiahuawang7@gmail.com>
2013-02-14 19:32:33 -0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoPlatform::Application.config.secret_token = 'a64c67afec48338a249f9fbe93e80d0207e4e748f98761f24ee0e04512822cd6420135663836ad82484f636c781d97ee5c9538f8f94bad64c663516957786d85'
